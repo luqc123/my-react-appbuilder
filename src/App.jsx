028a14b80d4ec4f8541f339e718ba6589efbf798
luqc123 <807599175@qq.com>
2024-04-19 15:55:26 +0800

    antd的Select test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import LoginForm from "./LoginForm.jsx";
+import { Select } from "antd";
+
+const { Option } = Select;
 
 //npm run dev start test
 function App() {
     return(
       <>
-      <LoginForm/>
+      <Select defaultValue="lucy" style={{ width: 120 }}>
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="disabled" disabled>
+        </Option>
+        <Option value="Yiminghe">yiminghe</Option>
+      </Select>
       </>
     );
 }
