545a7595509ef865c052aad79ea41b8e4a237fba
luqc123 <807599175@qq.com>
2024-04-19 15:51:12 +0800

    antd的Form test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import List from "./List"
+import React from "react";
+import LoginForm from "./LoginForm.jsx";
 
 //npm run dev start test
 function App() {
     return(
       <>
-        <List/>
+      <LoginForm/>
       </>
     );
 }
---
+++ src/LoginForm.jsx
@@ -0,0 +1,38 @@
+import { Form, Input, Button, } from "antd";
+
+const LoginForm = ()=>{
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Form name="basic"
+    initialValues={{remember:true}}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}>
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{required:true,message:"Please input your username!"}]}>
+          <Input/>
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{required:true,message:"Please input your password!"}]}>
+          <Input.Password/> 
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default LoginForm;
